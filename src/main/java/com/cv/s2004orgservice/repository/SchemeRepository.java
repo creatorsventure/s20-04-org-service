03125c01531979acf36934fb21ce0a3302766b08
dpak8 <dpak804@gmail.com>
2025-04-18 00:38:05 +0530

    commit on 18042025(BankUnit & Scheme)

---
+++ src/main/java/com/cv/s2004orgservice/repository/SchemeRepository.java
@@ -0,0 +1,13 @@
+package com.cv.s2004orgservice.repository;
+
+import com.cv.s10coreservice.repository.generic.GenericRepository;
+import com.cv.s10coreservice.repository.generic.GenericSpecification;
+import com.cv.s2002orgservicepojo.entity.Scheme;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface SchemeRepository extends GenericRepository, GenericSpecification<Scheme>,
+        JpaRepository<Scheme, String>, JpaSpecificationExecutor<Scheme> {
+}
