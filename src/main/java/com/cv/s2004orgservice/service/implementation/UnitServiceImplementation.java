003eb014e57ac2b181c16de09e87712a6620ddf8
ram <ram_r@live.com>
2025-04-22 21:08:19 +0400

    Commit 22-04-2025 - ORG Portal features

--- src/main/java/com/cv/s2004orgservice/service/implementation/UnitServiceImplementation.java
+++ src/main/java/com/cv/s2004orgservice/service/implementation/UnitServiceImplementation.java
@@ -5,9 +5,9 @@ import com.cv.s10coreservice.exception.ExceptionComponent;
 import com.cv.s10coreservice.service.function.StaticFunction;
 import com.cv.s10coreservice.util.StaticUtil;
 import com.cv.s2002orgservicepojo.dto.UnitDto;
-import com.cv.s2002orgservicepojo.entity.Unit;
+import com.cv.s2002orgservicepojo.entity.*;
 import com.cv.s2004orgservice.constant.ORGConstant;
-import com.cv.s2004orgservice.repository.UnitRepository;
+import com.cv.s2004orgservice.repository.*;
 import com.cv.s2004orgservice.service.intrface.UnitService;
 import com.cv.s2004orgservice.service.mapper.UnitMapper;
 import jakarta.transaction.Transactional;
@@ -34,10 +34,17 @@ public class UnitServiceImplementation implements UnitService {
     private final UnitMapper mapper;
     private final ExceptionComponent exceptionComponent;
 
+    private final ActionRepository actionRepository;
+    private final CurrencyRepository currencyRepository;
+    private final EngineRepository engineRepository;
+    private final OptionsRepository optionsRepository;
+
     @CacheEvict(keyGenerator = "cacheKeyGenerator", allEntries = true)
     @Override
     public UnitDto create(UnitDto dto) throws Exception {
-        return mapper.toDto(repository.save(mapper.toEntity(dto)));
+        var entity = mapper.toEntity(dto);
+        constructUnitEntity(dto, entity);
+        return mapper.toDto(repository.save(entity));
     }
 
     @CacheEvict(keyGenerator = "cacheKeyGenerator", allEntries = true)
@@ -45,9 +52,26 @@ public class UnitServiceImplementation implements UnitService {
     public UnitDto update(UnitDto dto) throws Exception {
         return mapper.toDto(repository.findById(dto.getId()).map(entity -> {
             BeanUtils.copyProperties(dto, entity);
+            try {
+                constructUnitEntity(dto, entity);
+            } catch (Exception e) {
+                log.error("Error while updating unit", e);
+                throw new RuntimeException(e);
+            }
             repository.save(entity);
             return entity;
         }).orElseThrow(() -> exceptionComponent.expose("app.message.failure.object.unavailable", true)));
+    }
+
+    private void constructUnitEntity(UnitDto dto, Unit entity) throws Exception {
+        entity.setActionList(actionRepository.findAllByStatusTrueAndIdIn(dto.getSelectedActionIds(), Action.class)
+                .orElseThrow(() -> exceptionComponent.expose("app.message.failure.object.unavailable", true)));
+        entity.setCurrencyList(currencyRepository.findAllByStatusTrueAndIdIn(dto.getSelectedCurrencyIds(), Currency.class)
+                .orElseThrow(() -> exceptionComponent.expose("app.message.failure.object.unavailable", true)));
+        entity.setEngineList(engineRepository.findAllByStatusTrueAndIdIn(dto.getSelectedEngineIds(), Engine.class)
+                .orElseThrow(() -> exceptionComponent.expose("app.message.failure.object.unavailable", true)));
+        entity.setOptions(optionsRepository.findByIdAndStatusTrue(dto.getSelectedOptionsId(), Options.class)
+                .orElseThrow(() -> exceptionComponent.expose("app.message.failure.object.unavailable", true)));
     }
 
     @CacheEvict(keyGenerator = "cacheKeyGenerator", allEntries = true)
@@ -63,8 +87,16 @@ public class UnitServiceImplementation implements UnitService {
     @Cacheable(keyGenerator = "cacheKeyGenerator")
     @Override
     public UnitDto readOne(String id) throws Exception {
-        return mapper.toDto(repository.findByIdAndStatusTrue(id, Unit.class)
-                .orElseThrow(() -> exceptionComponent.expose("app.message.failure.object.unavailable", true)));
+        return repository.findByIdAndStatusTrue(id, Unit.class)
+                .map(entity -> {
+                    var dto = mapper.toDto(entity);
+                    dto.setSelectedActionIds(entity.getActionList().stream().map(Action::getId).collect(Collectors.toList()));
+                    dto.setSelectedCurrencyIds(entity.getCurrencyList().stream().map(Currency::getId).collect(Collectors.toList()));
+                    dto.setSelectedEngineIds(entity.getEngineList().stream().map(Engine::getId).collect(Collectors.toList()));
+                    dto.setSelectedOptionsId(entity.getOptions().getId());
+                    return dto;
+                })
+                .orElseThrow(() -> exceptionComponent.expose("app.message.failure.object.unavailable", true));
     }
 
     @CacheEvict(keyGenerator = "cacheKeyGenerator", allEntries = true)
